ce93b9c316c84ca92de9a0f7c84a246c11f1add1
杨再涛 <1772081518@qq.com>
2024-12-19 21:20:07 +0800

    更改目录结构

--- script/GameMain.cs
+++ script/GameMain.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class GameMain : Node
 {
+	[Export]
+	Node ui, context;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
