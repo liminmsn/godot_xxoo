46a2ed651557a120f1f1f45b02c1377a010276e5
杨再涛 <1772081518@qq.com>
2024-12-22 19:34:00 +0800

    完下棋逻辑

--- script/bases/GameBase.cs
+++ script/bases/GameBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Godot;
 public abstract partial class GameBase : Node
 {
@@ -20,7 +21,8 @@ public abstract partial class GameBase : Node
     PackedScene X, O;
     private bool IsX;
     private RigidBody3D[] Pawns = new RigidBody3D[9];
-    private void AddPawn(uint idx)
+    //下棋
+    private async void AddPawn(uint idx)
     {
         if (GetPawn(idx) == null)
         {
@@ -34,10 +36,94 @@ public abstract partial class GameBase : Node
             if (IsWin(xo_pawn) != null) //判断当前棋子是否赢
             {
                 GD.Print($"{xo_pawn.GetMeta("type")} 获胜！！！");
+                await Task.Delay(500);//延迟一秒
+                DestoryAll();
             }
             else //下一个玩家走棋子
             {
                 IsX = !IsX;
+            }
+        }
+    }
+    //判断当前下的棋子是否赢
+    private RigidBody3D IsWin(RigidBody3D pawn)
+    {
+        /**
+        6 | 7 | 8
+        -----------
+        3 | 4 | 5
+        -----------
+        0 | 1 | 2
+        */
+        RigidBody3D pawn_win = null;
+        string[][] ar_x = { new string[3], new string[3], new string[3] };
+        string[][] ar_y = { new string[3], new string[3], new string[3] };
+        string[][] ar_xyx = { new string[3], new string[3] };
+        string[][][] ar_all = { ar_x, ar_y, ar_xyx };
+        for (uint y = 0; y < 3; y++)
+        {
+            //x轴线判断胜利的玩家
+            for (uint x = 0; x < 3; x++)
+            {
+                var pawn_x = GetPawn(y * 3 + x);
+                var pawn_y = GetPawn(x * 3 + y);
+                if (pawn_x != null)
+                {
+                    ar_x[y][x] = (string)pawn_x.GetMeta("type");
+                }
+                if (pawn_y != null)
+                {
+                    ar_y[y][x] = (string)pawn_y.GetMeta("type");
+                }
+            }
+        }
+
+        //斜线判断
+        for (uint idx = 3; idx > 0; idx--)
+        {
+            for (uint x = 0; x < 3; x++)
+            {
+                if (idx == 3)
+                {
+                    var ar_xy = GetPawn(idx * x + x);
+                    if (ar_xy != null)
+                    {
+                        ar_xyx[0][x] = (string)ar_xy.GetMeta("type");
+                    }
+                }
+                if (idx == 2)
+                {
+                    var ar_yx = GetPawn(idx * (x + 1));
+                    if (ar_yx != null)
+                    {
+                        ar_xyx[1][x] = (string)ar_yx.GetMeta("type");
+                    }
+                }
+            }
+        }
+
+        Array.ForEach(ar_all, ar =>
+           {
+               for (uint y = 0; y < ar.Length; y++)
+               {
+                   if (ar[y].All(val => val == ar[y][0] && val != null))
+                   {
+                       pawn_win = pawn;
+                   }
+               }
+           });
+        return pawn_win;
+    }
+    //清空棋盘
+    private void DestoryAll()
+    {
+        for (uint i = 0; i < Pawns.Length; i++)
+        {
+            if (Pawns[i] != null)
+            {
+                RemoveChild(Pawns[i]);
+                Pawns[i] = null;
+                GD.Print(Pawns[i]);
             }
         }
     }
@@ -60,52 +146,6 @@ public abstract partial class GameBase : Node
         return Pawns[idx];
     }
 
-    protected RigidBody3D IsWin(RigidBody3D pawn)
-    {
-        /**
-        6 | 7 | 8
-        -----------
-        3 | 4 | 5
-        -----------
-        0 | 1 | 2
-        */
-        string[][] ar_x = { new string[3], new string[3], new string[3] };
-        string[][] ar_y = { new string[3], new string[3], new string[3] };
-        string[][] ar_xyx = { new string[3], new string[3], new string[3] };
-        string[][][] ar_all = { ar_x, ar_y, ar_xyx };
-        RigidBody3D pawn_win = null;
-
-        for (uint y = 0; y < 3; y++)
-        {
-            //x轴线判断胜利的玩家
-            for (uint x = 0; x < 3; x++)
-            {
-                var pawn_x = GetPawn(y * 3 + x);
-                var pawn_y = GetPawn(x * 3 + y);
-                if (pawn_x != null)
-                {
-                    ar_x[y][x] = (string)pawn_x.GetMeta("type");
-                }
-                if (pawn_y != null)
-                {
-                    ar_y[y][x] = (string)pawn_y.GetMeta("type");
-                }
-            }
-            for (uint idx = 0; idx < 2; idx++)
-            {
-
-            }
-
-            Array.ForEach(ar_all, ar =>
-            {
-                if (ar[y].All(val => val == ar[y][0] && val != null))
-                {
-                    pawn_win = pawn;
-                }
-            });
-        }
-        return pawn_win;
-    }
 
     //随机bol值
     protected void RandomIsX()
